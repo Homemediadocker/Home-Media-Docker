9512359935397e5dff1b8c50c71b0ce525b79091
Homemediadocker <167828008+Homemediadocker@users.noreply.github.com>
2024-06-09 21:43:29 -0400

    Feature/separating env files (#4)
    * Added all new file structure - Separated global env
    
    * The project was getting very large so it became necessary to separate the .env file into smaller pieces.
    * Each container has its own associated env.
    * There is still a global env for the shared settings across all the containers.
    
    * Added new script to copy the .env-example files into a .env that can be modified and is already gitignored.
    * This can be run once and never run again, but can also be run each time a new service yaml file is added and it will only create a .env if it doesn't currently exist.
    
    * Added copyenv script to the emby workflow. updated emby profile on the script
    
    * Added to the mattermost env file
    
    * Added Wallos - personal subscription tracker
    
    * Added compose profiles
    
    * updated .env
    
    * updated startup script action
    
    * Updated env script in the startup action
    
    * updated .env
    
    * added ls
    
    * updated script to not have .env-test
    
    * removing ls
    
    * added profile back to emby script
    
    * Added copy env to the actions scripts
    
    * Added to .env-example
    
    * Added vaultwarden
    
    * Added change me's in places that needed it

---
+++ scripts/copyenv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ ! -f .env ]; then
+  echo "Global .env not found - Creating global .env file"
+  cp .env-example .env
+fi
+
+
+
+for f in ./compose-files/*/; do
+  if [ ! -f "$f.env" ]; then
+    echo "CREATE - $f.env"
+    cp "$f.env-example" "$f.env"
+  fi
+  # echo "Copying .env-example into .env for $f"
+  # cp "$f/.env-example" "$f/.env"
+done
